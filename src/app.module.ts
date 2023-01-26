d08c5c110c9d5cca5507f804ebc29edba8297661
Clément Llorens <bk201li@protonmail.com>
2023-01-26 15:46:41 +0100

    post likes done

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { PostLikesModule } from './post-likes/post-likes.module';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [UsersModule, PostsModule, PostLikesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })

aa43fb7b12a0ae03043bc36c6f13f800eb15c82d
Etienne Bachelard <newcorp.pro@gmail.com>
2023-02-09 10:46:45 +0100

    stranger key user

--- src/post-comments/post-comments.service.ts
+++ src/post-comments/post-comments.service.ts
@@ -4,30 +4,36 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PostCommentsService {
-    create(userId: string, postId: string, CreatePostCommentDto: CreatePostCommentDto) {
-        const prisma = new PrismaClient();
-        return prisma.postComment.create({
-            data: {
-                text: CreatePostCommentDto.text,
-                userId: parseInt(userId),
-                postId: parseInt(postId),
-            },
-        });
-    }
+  create(
+    userId: string,
+    postId: string,
+    CreatePostCommentDto: CreatePostCommentDto,
+  ) {
+    const prisma = new PrismaClient();
+    return prisma.postComment.create({
+      data: {
+        text: CreatePostCommentDto.text,
+        userId: parseInt(userId),
+        postId: parseInt(postId),
+      },
+    });
+  }
 
-    findAll(postId: string, limit: string) {
-        const prisma = new PrismaClient();
-        return prisma.postComment.findMany({
-            take: typeof limit === 'string' ? parseInt(limit) : undefined,
-            orderBy: [
-                {
-                    createdAt: 'desc',
-                },
-            ], 
-            where: { 
-                postId: parseInt(postId),
-            },
-        });
-    }
-
+  findAll(postId: string, limit: string) {
+    const prisma = new PrismaClient();
+    return prisma.postComment.findMany({
+      take: typeof limit === 'string' ? parseInt(limit) : undefined,
+      orderBy: [
+        {
+          createdAt: 'desc',
+        },
+      ],
+      where: {
+        postId: parseInt(postId),
+      },
+      include: {
+        user: true,
+      },
+    });
+  }
 }
